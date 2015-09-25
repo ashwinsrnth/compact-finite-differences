95134d6b498894b9d193d0744523bc5c51040222
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-09-24 22:06:56 -0400

    add kernel to compute dfdy RHS:

--- code/compact/kernels.cl
+++ code/compact/kernels.cl
@@ -68,8 +68,6 @@ __kernel void computeRHSdfdx(__global double *f_local_d,
 
     rhs_d[i] = (3./(4*dx))*(f_local_d[iloc+1] - f_local_d[iloc-1]);
 
-
-
     if (mx == 0) {
         if (ix == 0) {
             rhs_d[i] = (1./(2*dx))*(-5*f_local_d[iloc] + 4*f_local_d[iloc+1] + f_local_d[iloc+2]);
@@ -82,6 +80,53 @@ __kernel void computeRHSdfdx(__global double *f_local_d,
         }
     }
 }
+
+
+__kernel void computeRHSdfdy(__global double *f_local_d,
+                        __global double *rhs_d,
+                        double dy,
+                        int nx,
+                        int ny,
+                        int nz,
+                        int my,
+                        int npy)
+{
+    /*
+    Computes the RHS for solving for the y-derivative
+    of a function f. f_local is the "local" part of
+    the function which includes ghost points.
+
+    dy is the spacing.
+
+    nx, ny, nz define the size of d. f_local is shaped
+    [nz+2, ny+2, nx+2]
+
+    my and npy together decide if we are evaluating
+    at a boundary.
+    */
+
+    int ix = get_global_id(0);
+    int iy = get_global_id(1);
+    int iz = get_global_id(2);
+
+    int i = iz*(nx*ny) + iy*nx + ix;
+    int iloc = (iz+1)*((nx+2)*(ny+2)) + (iy+1)*(nx+2) + (ix+1);
+
+    rhs_d[i] = (3./(4*dy))*(f_local_d[iloc+(nx+2)] - f_local_d[iloc-(nx+2)]);
+
+    if (my == 0) {
+        if (iy == 0) {
+            rhs_d[i] = (1./(2*dy))*(-5*f_local_d[iloc] + 4*f_local_d[iloc+(nx+2)] + f_local_d[iloc+2*(nx+2)]);
+        }
+    }
+
+    if (my == npy-1) {
+        if (iy == ny-1) {
+            rhs_d[i] = -(1./(2*dy))*(-5*f_local_d[iloc] + 4*f_local_d[iloc-(nx+2)] + f_local_d[iloc-2*(nx+2)]);
+        }
+    }
+}
+
 
 __kernel void sumSolutionsdfdx3D(__global double* x_R_d,
                             __global double* x_UH_d,
--- code/compact/test_kernels.py
+++ code/compact/test_kernels.py
@@ -7,6 +7,7 @@ from numpy.testing import *
 import pThomas
 import sharedCyclicReduction
 import kernels
+import mpiDA
 
 def scipy_solve_banded(a, b, c, rhs):
     '''
@@ -28,6 +29,62 @@ platform = cl.get_platforms()[0]
 device = platform.get_devices()[0]
 context = cl.Context([device])
 queue = cl.CommandQueue(context)
+
+def test_RHS_dfdx():
+    nz = 4
+    ny = 8
+    nx = 16
+    f = np.random.rand(nz+2, ny+2, nx+2)
+    dx = 1.0
+    r_true = np.zeros([nz, ny, nx])
+    r_true[:, :, :] = (3./(4*dx))*(f[1:-1, 1:-1, 2:] - f[1:-1, 1:-1, :-2])
+    r_true[:, :, 0] = (1./(2*dx))*(
+            -5*f[1:-1, 1:-1, 1] +
+            4*f[1:-1, 1:-1, 2] +
+            f[1:-1, 1:-1, 3])
+    r_true[:, :, -1] = -(1./(2*dx))*(
+            -5*f[1:-1, 1:-1, -2] +
+            4*f[1:-1, 1:-1, -3] +
+            f[1:-1, 1:-1, -4])
+    r = np.zeros_like(r_true, dtype=np.float64)
+    f_d = cl_array.to_device(queue, f)
+    r_d = cl_array.to_device(queue, r)
+    compute_RHS_dfdx, = kernels.get_funcs(context, 'kernels.cl',
+            'computeRHSdfdx')
+    compute_RHS_dfdx(queue, [nx, ny, nz], None,
+            f_d.data, r_d.data, np.float64(dx),
+                np.int32(nx), np.int32(ny), np.int32(nz),
+                    np.int32(0), np.int32(1))
+    r = r_d.get()
+    assert_allclose(r, r_true)
+
+def test_RHS_dfdy():
+    nz = 4
+    ny = 16
+    nx = 8
+    f = np.random.rand(nz+2, ny+2, nx+2)
+    dy = 1.0
+    r_true = np.zeros([nz, ny, nx])
+    r_true[:, :, :] = (3./(4*dy))*(f[1:-1, 2:, 1:-1] - f[1:-1, :-2, 1:-1])
+    r_true[:, 0, :] = (1./(2*dy))*(
+            -5*f[1:-1, 1, 1:-1] +
+            4*f[1:-1, 2, 1:-1] +
+            f[1:-1, 3, 1:-1])
+    r_true[:, -1, :] = -(1./(2*dy))*(
+            -5*f[1:-1, -2, 1:-1] +
+            4*f[1:-1, -3, 1:-1] +
+            f[1:-1, -4, 1:-1])
+    r = np.zeros_like(r_true, dtype=np.float64)
+    f_d = cl_array.to_device(queue, f)
+    r_d = cl_array.to_device(queue, r)
+    compute_RHS_dfdy, = kernels.get_funcs(context, 'kernels.cl',
+            'computeRHSdfdy')
+    compute_RHS_dfdy(queue, [nx, ny, nz], None,
+            f_d.data, r_d.data, np.float64(dy),
+                np.int32(nx), np.int32(ny), np.int32(nz),
+                    np.int32(0), np.int32(1))
+    r = r_d.get()
+    assert_allclose(r, r_true)
 
 def test_copy_faces():
     nz = 3
