6d921085546648c616e7e3bb64d9e3eb27a02e01
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-09-27 00:39:12 -0400

    add some tests and a makefile

--- code/test_compact.py
+++ code/test_compact.py
@@ -1,21 +1,44 @@
 import numpy as np
 from mpi4py import MPI
-
 from mpi_util import *
 from compact import CompactFiniteDifferenceSolver
 
-if __name__ == "__main__":
+from numpy.testing import *
+
+def test_sine_regular():
     comm = MPI.COMM_WORLD 
-    da = DA(comm, (32, 8, 16), (2, 2, 2), 1)
+    da = DA(comm, (8, 8, 8), (2, 2, 2), 1)
     x, y, z = DA_arange(da, (0, 2*np.pi), (0, 2*np.pi), (0, 2*np.pi))
-    f = x*np.sin(y) + z*np.cos(y*x)
-    dfdx_true = np.sin(y) - z*y*np.sin(y*x)
+    f = np.sin(x) 
+    dfdx_true = np.cos(x) 
     dx = x[0, 0, 1] - x[0, 0, 0]
     cfd = CompactFiniteDifferenceSolver(da)
     dfdx = cfd.dfdx(f, dx)
-    
-    import matplotlib.pyplot as plt
-    if da.rank == 0:
-        plt.plot(dfdx_true[3, 3, :])
-        plt.plot(dfdx[3, 3, :], '-o')
-        plt.savefig('temp.png')
+    assert_almost_equal(dfdx_true, dfdx, decimal=2)
+
+def test_sine_irregular():
+    comm = MPI.COMM_WORLD 
+    da = DA(comm, (8, 32, 16), (2, 2, 2), 1)
+    x, y, z = DA_arange(da, (0, 2*np.pi), (0, 2*np.pi), (0, 2*np.pi))
+    f = np.sin(x) 
+    dfdx_true = np.cos(x) 
+    dx = x[0, 0, 1] - x[0, 0, 0]
+    cfd = CompactFiniteDifferenceSolver(da)
+    dfdx = cfd.dfdx(f, dx)
+    assert_almost_equal(dfdx_true, dfdx, decimal=2)
+
+def test_xyz():
+    comm = MPI.COMM_WORLD 
+    da = DA(comm, (8, 32, 16), (2, 2, 2), 1)
+    x, y, z = DA_arange(da, (0, 2*np.pi), (0, 2*np.pi), (0, 2*np.pi))
+    f = x*y*z
+    dfdx_true = y*z
+    dx = x[0, 0, 1] - x[0, 0, 0]
+    cfd = CompactFiniteDifferenceSolver(da)
+    dfdx = cfd.dfdx(f, dx)
+    assert_almost_equal(dfdx_true, dfdx, decimal=2)
+
+if __name__ == "__main__":
+    test_sine_regular()
+    test_sine_irregular()
+    test_xyz()
