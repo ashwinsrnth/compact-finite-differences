e1d922d68ade7b76fbfdfb163832b1b054dd8a77
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-10-02 13:40:47 -0400

    move test_mpi_util.py

--- code/test/test_mpi_util/test_misc.py
+++ code/test/test_mpi_util/test_misc.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../../')
 from mpi_util import *
 import numpy as np
 from mpi4py import MPI
@@ -15,14 +17,14 @@ def test_DA_arange():
         for i in range(5):
             for j in range(5):
                 assert_equal(x[i, j, :], np.arange(6, 11))
-
+    print 'pass'
 def test_DA_get_line_DA():
     da = DA(comm, [5, 5, 5], [2, 2, 2], 1)
     line_da = da.get_line_DA(0)
     assert(line_da.npx == 2)
     assert(line_da.npy == 1)
     assert(line_da.npz == 1)
-
+    print 'pass'
 def test_DA_gather():
     da = DA(comm, [5, 5, 5], [2, 2, 2], 1)
     a = da.create_global_vector()
@@ -33,7 +35,7 @@ def test_DA_gather():
         assert_equal(a_root, np.arange(8))
     else:
         assert_equal(a_root, 0)
-
+    print 'pass'
 if __name__ == "__main__":
     test_DA_arange()
     test_DA_get_line_DA()
